23203d9a28598cd3c733ed982d7c9b61e8584a44
ASUS <farzullasoy.tengiz@gmail.com>
2023-05-30 22:45:20 +0400

    yeni cap

---
+++ src/main/java/com/example/huseyn/HuseynApplication.java
@@ -0,0 +1,13 @@
+package com.example.huseyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuseynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuseynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/huseyn/HuseynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huseyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuseynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
